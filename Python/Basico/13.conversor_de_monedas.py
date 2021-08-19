4897fc87eb3c3195dd45e6c96c174fcf1c930ad5
Daniel Angel <dani.angel2002@hotmail.com>
2021-08-18 22:27:13 -0500

    Adelnato hasta el video 22

--- Python/Basico/13.conversor_de_monedas.py
+++ Python/Basico/13.conversor_de_monedas.py
@@ -1,3 +1,12 @@
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("How many" + tipo_pesos + "pesos do you have? ")
+    pesos = float(pesos)
+    dolares= pesos / valor_dolar
+    dolares = round(dolares, 2) #Tomamos el contenido de la variable dolar y lo reducimos a dos decimales 
+    dolares = str(dolares)  #Convertimos el valor de dolares a texto o string
+    print(" Tienes $" + dolares + " dolares ")
+
+
 menu = """ Bienvenidos al conversor de monedas 
 
         1-Pesos colombianos
@@ -9,31 +18,13 @@ menu = """ Bienvenidos al conversor de monedas
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = input("How many Colombian pesos do you have? ")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares= pesos / valor_dolar
-    dolares = round(dolares, 2) #Tomamos el contenido de la variable dolar y lo reducimos a dos decimales 
-    dolares = str(dolares)  #Convertimos el valor de dolares a texto o string
-    print(" Tienes $" + dolares + " dolares ")
+    conversor("colombianos", 3875)
 
 elif opcion ==2:
-    pesos = input("How many Argentine pesos do you have? ")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares= pesos / valor_dolar
-    dolares = round(dolares, 2) #Tomamos el contenido de la variable dolar y lo reducimos a dos decimales 
-    dolares = str(dolares)  #Convertimos el valor de dolares a texto o string
-    print(" Tienes $" + dolares + " dolares ")
+    conversor("argentinos", 65)
 
 elif opcion ==3:
-    pesos = input("How many mexican pesos do you have? ")
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares= pesos / valor_dolar
-    dolares = round(dolares, 2) #Tomamos el contenido de la variable dolar y lo reducimos a dos decimales 
-    dolares = str(dolares)  #Convertimos el valor de dolares a texto o string
-    print(" Tienes $" + dolares + " dolares ")
+    conversor("mexicanos", 24)
 
 else:
     print("Ingresa una opcion correcta ")
