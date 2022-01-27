7f6637d04485d9342dddd1610efc5151ec73b5af
itsmagicsz <daniangel2002@hotmail.com>
2022-01-27 13:13:36 -0500

    Inicializacion del curso

--- JavaScript/Curso Básico de JavaScript/Modulo 7 Metodos de Arrays/metodos_eliminacion_array.js
+++ JavaScript/Curso Básico de JavaScript/Modulo 7 Metodos de Arrays/metodos_eliminacion_array.js
@@ -1,4 +1,11 @@
 /*.push()
 El método .push() nos permite agregar uno o más elementos al final de un array.
 
-A continuación veremos un ejemplo aplicado con un array que contiene números:*/
+A continuación veremos un ejemplo aplicado con un array que contiene números:
+
+Agregar al final= .push()
+Eliminar al final= .pop()
+
+Agregar al inicio= .unshift()
+Eliminar al inicio= .shift()
+*/
